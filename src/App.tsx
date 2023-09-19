ac9c6f188d7d8c4fd9c7d95c206812d241428750
102sae <102chimee@gmail.com>
2023-09-19 17:04:40 +0900

    add router

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@chakra-ui/react"
-
-function App() {
-  return <div>
-    <Text color={"gray.600"}fontSize={"8xl"}>It works</Text>
-  </div>
-};
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import router from './router';
+import { RouterProvider } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+
 root.render(
   <React.StrictMode>
    <ChakraProvider>
-   <App />
+    <RouterProvider router={router}/>
    </ChakraProvider>
   </React.StrictMode>
 );
